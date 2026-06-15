0313c94f2194d6a31acfc7182fac2a999207a4a8
Aksh Talesara <akshtalesara2008@gmail.com>
2026-06-15 19:15:37 +0530

    This is the first version of calculator

---
+++ calculator.py
@@ -0,0 +1,6 @@
+print("                                    ** Welcome to the calculator program**                          \nThis program can perform basic arithmetic operations like addition, subtraction, multiplication, and division.  ")
+a = int(input("Please enter the first number: "))
+b= int(input("Please enter the second number: "))
+print("The value of", a ,"+", b, "is = ", a+b)
+
+
