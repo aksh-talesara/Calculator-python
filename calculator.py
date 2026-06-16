ecebefab3d745d776fbd4fe3976a77792c4f195e
Aksh Talesara <akshtalesara2008@gmail.com>
2026-06-16 12:59:34 +0530

    Added new features like multiplication, division etc

--- calculator.py
+++ calculator.py
@@ -1,6 +1,20 @@
 print("                                    ** Welcome to the calculator program**                          \nThis program can perform basic arithmetic operations like addition, subtraction, multiplication, and division.  ")
 a = int(input("Please enter the first number: "))
 b= int(input("Please enter the second number: "))
-print("The value of", a ,"+", b, "is = ", a+b)
+op = input("Enter the operator (+,-,* or /) ")
+
+match op:
+    case "+":
+        print("The value of", a, "+", b, "is equals to",a+b)
+    case "-":
+        print("The value of", a, "-", b, "is equals to",a-b)
+    case "*":
+        print("The value of", a, "*", b, "is equals to",a*b)
+    case "+":
+        print("The value of", a, "x", b, "is equals to",a*b)
+    case "/":
+        print("The value of", a, "+", b, "is equals to",a/b)
+    case _:
+        print("invalid")
 
 
