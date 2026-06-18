6736e32346e2f9c4968208fc4ecbdd07b14fb67e
Aksh Talesara <akshtalesara2008@gmail.com>
2026-06-18 22:16:33 +0530

    Improved error handling (v2.1)

--- calculator.py
+++ calculator.py
@@ -1,22 +1,27 @@
 print("                                    ** Welcome to the calculator program**                          \nThis program can perform basic arithmetic operations like addition, subtraction, multiplication, and division.  ")
-a = int(input("Please enter the first number: "))
-b= int(input("Please enter the second number: "))
-op = input("Enter the operator (+,-,* or /) ")
+try:
+    a = int(input("Please enter the first number: "))
 
-match op:
-    case "+":
-        print("The value of", a, "+", b, "is equals to",a+b)
-    case "-":
-        print("The value of", a, "-", b, "is equals to",a-b)
-    case "*":
-        print("The value of", a, "*", b, "is equals to",a*b)
-    case "+":
-        print("The value of", a, "x", b, "is equals to",a*b)
-    case "/":
-        print("The value of", a, "+", b, "is equals to",a/b)
-    case _:
-        print("invalid")
-
+    b= int(input("Please enter the second number: "))
+    op = input("Enter the operator (+,-,* or /) ")
+except:
+    print("error invalid")
+try:
+    match op:
+        case "+":
+            print("The value of", a, "+", b, "is equals to",a+b)
+        case "-":
+            print("The value of", a, "-", b, "is equals to",a-b)
+        case "*":
+            print("The value of", a, "*", b, "is equals to",a*b)
+        case "+":
+            print("The value of", a, "x", b, "is equals to",a*b)
+        case "/":
+            print("The value of", a, "+", b, "is equals to",a/b)
+        case _:
+            print("invalid")
+except:
+    pass
 #this is the latest version2 as of 16 June 2026
 
 
